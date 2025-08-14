0bfb52f453973f0a45bd9bfcc37f7c2e7e7b89bb
mendi kuperman <mendikuperman@gmail.com>
2025-08-14 17:16:22 +0300

    implement dockerfile  and scripts to mysql

---
+++ scripts/SQL/data_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO data (first_name, last_name) VALUES
+('mendy', 'kuperman'),
+('moshe', 'cohen'),
+('avi', 'zalmanov'),
+('david', 'levy'),
+('yosef', 'shapiro');
--- services/dal/db_connector.py
+++ services/dal/db_connector.py
@@ -8,21 +8,15 @@ class DBConnector:
 
     def __init__(self):
 
-        host = os.getenv("MYSQL_HOST")
+        host = os.getenv("MYSQL_URL")
         user = os.getenv("MYSQL_USER")
-        port_str = os.getenv("MYSQL_PORT")
         password = os.getenv("MYSQL_PASSWORD")
-        database = os.getenv("MYSQL_DB")
+        database = os.getenv("MYSQL_DATABASE")
 
-        try:
-            port = int(port_str)
-        except (ValueError, TypeError):
-            raise ValueError(f"Invalid port: {port_str}. Make sure MYSQL_PORT is a valid number in your .env file.")
 
         self.conn = mysql.connector.connect(
             host=host,
             user=user,
-            port=port,
             password=password,
             database=database
         )
